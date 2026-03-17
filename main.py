0152a04849257e90b9006c9ed9cbca7a607dab68
Colton Chan <coltonlhchan@gmail.com>
2026-03-17 15:57:08 -0700

    fixed python version and reinstalled dependencies

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from langchain.chains import LLMChain
-from langchain_core.prompts import PromptTemplate
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 
 
+model = OllamaLLM(model="llama3.2")
+
+template="You are an rules expert that answers questions about the rules of ultimate frisbee. " \
+"Here are the rules: {rules}. Answer the following question: {question}" 
+
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain= prompt | model
+
+result=chain.invoke({"rules":[], "question":"What is the maximum number of players on the field for each team?"})
+
+print(result)
